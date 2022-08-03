4494c4a566ba1f53fe61f2097af758ef9eeff325
Tom Segev <tom@eve.io>
2022-08-03 20:32:39 +0200

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <MainTodos />
+</template>
+
+<script lang="ts">
+import MainTodos from '@/views/MainTodos.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    MainTodos
+  }
+}
+</script>
