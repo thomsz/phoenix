1e5e462293d51d1a8b1338a629ac57869ea98926
Tom Segev <tom@eve.io>
2022-08-05 15:25:19 +0200

    feat: refactor - extract component

--- client/src/components/NewTodo.vue
+++ client/src/components/NewTodo.vue
@@ -14,48 +14,15 @@
       />
       <ReturnIcon
         v-if="expanded"
-        class="w-10 mx-2 fill-slate-100 hidden md:block"
+        class="w-10 mx-2 fill-slate-200 hidden md:block"
       />
     </div>
     <Transition name="fade">
-      <div
+      <NewTodoModal
         v-if="expanded"
-        class="flex flex-col space-y-3 absolute w-full bg-white/90 backdrop-blur-sm top-16"
-      >
-        <hr />
-        <textarea
-          name="description"
-          rows="5"
-          class="w-full p-2 bg-transparent"
-          v-model="form.description"
-          placeholder="Description..."
-        >
-        </textarea>
-        <label class="px-2 text-gray-400 hover:cursor-pointer">
-          Due date:
-          <input
-            name="due_date"
-            type="date"
-            class="h-8 bg-transparent"
-            v-model="form.dueDate"
-          />
-        </label>
-        <div class="w-full flex gap-2">
-          <button
-            class="action bg-blue-100 hover:bg-blue-200 grow"
-            :class="{ 'bg-blue-400 hover:bg-blue-500': form.title }"
-          >
-            Create task
-          </button>
-          <button
-            type="button"
-            class="action w-12 bg-gray-200 hover:bg-gray-300"
-            @click="expanded = false"
-          >
-            <XIcon class="icon" />
-          </button>
-        </div>
-      </div>
+        v-model="form"
+        @close-button-clicked="expanded = false"
+      />
     </Transition>
   </form>
 </template>
@@ -63,9 +30,9 @@
 <script lang="ts">
 import { v4 as uuid } from 'uuid'
 import { defineComponent } from 'vue'
-import { XIcon } from '@heroicons/vue/solid'
 
 import ReturnIcon from '@/components/icons/ReturnIcon.vue'
+import NewTodoModal from '@/components/NewTodo/NewTodoModal.vue'
 
 import type Todo from '@/types/Todo'
 
@@ -79,8 +46,8 @@ export default defineComponent({
   name: 'NewTodo',
 
   components: {
-    XIcon,
-    ReturnIcon
+    ReturnIcon,
+    NewTodoModal
   },
 
   data: () => ({
@@ -123,11 +90,6 @@ export default defineComponent({
 <style scoped>
 form {
   @apply flex flex-col relative z-10;
-}
-
-button.action {
-  @apply flex justify-center items-center h-12 px-3 rounded-md font-bold uppercase text-white transition-colors
-    duration-500;
 }
 
 .fade-enter-active {
---
+++ client/src/components/NewTodo/NewTodoModal.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="flex flex-col space-y-3 absolute w-full bg-white/90 backdrop-blur-sm top-16">
+    <hr />
+    <textarea
+      name="description"
+      rows="5"
+      class="w-full p-2 bg-transparent"
+      v-model="form.description"
+      placeholder="Description..."
+    >
+    </textarea>
+    <label class="px-2 text-gray-400 hover:cursor-pointer">
+      Due date:
+      <input
+        name="due_date"
+        type="date"
+        class="h-8 bg-transparent"
+        v-model="form.dueDate"
+      />
+    </label>
+    <div class="w-full flex gap-2">
+      <button
+        class="action bg-blue-100 hover:bg-blue-200 grow"
+        :class="{ 'bg-blue-400 hover:bg-blue-500': form.title }"
+      >
+        Create task
+      </button>
+      <button
+        type="button"
+        class="action w-12 hover:bg-gray-300"
+        @click="$emit('close-button-clicked')"
+      >
+        <XIcon class="close icon fill-slate-300" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { XIcon } from '@heroicons/vue/solid'
+
+export default defineComponent({
+  name: 'NewTodoModal',
+
+  components: {
+    XIcon
+  },
+
+  props: {
+    modelValue: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      form: this.modelValue
+    }
+  },
+
+  watch: {
+    form: {
+      deep: true,
+      handler (form): void {
+        this.$emit('update:modelValue', form)
+      }
+    }
+  }
+})
+</script>
+
+<style scoped>
+button.action {
+  @apply flex justify-center items-center h-12 px-3 rounded-md font-bold uppercase text-white transition-colors
+    duration-500;
+}
+
+button.action:hover .icon.close {
+  @apply fill-white;
+}
+</style>
