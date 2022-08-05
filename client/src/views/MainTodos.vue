089b23b24745a225c31c52338d0dfb1a07cb5dfe
Tom Segev <tom@eve.io>
2022-08-05 13:43:07 +0200

    feat: add search by title

--- client/src/views/MainTodos.vue
+++ client/src/views/MainTodos.vue
@@ -1,14 +1,24 @@
 <template>
   <main>
     <NewTodo @todo-created="addItem" />
-    <div class="list">
+    <SearchBar v-model="searchQuery" />
+    <div
+      v-if="filteredTodos.length"
+      class="divide-y"
+    >
       <TodoItem
-        v-for="item in todos"
+        v-for="item in filteredTodos"
         :key="item.id"
         :item="item"
         @item-changed="updateItem"
         @delete-item-clicked="deleteItem(item)"
       />
+    </div>
+    <div
+      v-else
+      class="mx-auto py-16 text-slate-400"
+    >
+      No tasks to show
     </div>
   </main>
 </template>
@@ -18,6 +28,7 @@ import { defineComponent } from 'vue'
 
 import NewTodo from '@/components/NewTodo.vue'
 import TodoItem from '@/components/TodoItem.vue'
+import SearchBar from '@/components/SearchBar.vue'
 
 import type Todo from '@/types/Todo'
 
@@ -26,12 +37,21 @@ export default defineComponent({
 
   components: {
     NewTodo,
-    TodoItem
+    TodoItem,
+    SearchBar
   },
 
   data: () => ({
-    todos: [] as Array<Todo>
+    todos: [] as Array<Todo>,
+    searchQuery: ''
   }),
+
+  computed: {
+    filteredTodos (): Array<Todo> {
+      const items = [...this.todos]
+      return items.filter((todo: Todo) => todo.title.toLowerCase().includes(this.searchQuery.toLowerCase())).reverse()
+    }
+  },
 
   mounted (): void {
     this.fetchTodos()
